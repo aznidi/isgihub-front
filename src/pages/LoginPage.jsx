2f628af6b68002ae2996baaa5d25c192ae143ea3
Salah SAS <salahaznidi09@gmail.com>
2024-11-15 12:52:42 +0100

    Add Login Form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,16 @@
+// src/pages/LoginPage.jsx
+import React from 'react';
+import LoginForm from '../components/LoginForm';
 
-function LoginPage() {
+const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 overflow-hidden">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
+        <h1 className="text-2xl font-bold mb-4 text-center">Connexion</h1>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
